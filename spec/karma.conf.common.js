0cf972a32a43c72084bc57065d4fa6357e3b0682
shianqi <shianqi@imudges.com>
2017-08-30 21:45:50 +0800

    :bug: Fix Travis CI node 8.0 problem

--- spec/karma.conf.common.js
+++ spec/karma.conf.common.js
@@ -33,7 +33,7 @@ module.exports = {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'saucelabs'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
