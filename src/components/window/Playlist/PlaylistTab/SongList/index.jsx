fd2a4908ecf179a4c3c58cb8b5f4d40549e47a81
shianqi <shianqi@imudges.com>
2017-09-27 19:53:33 +0800

    :lipstick: Perfect song style

--- src/components/window/Playlist/PlaylistTab/SongList/index.jsx
+++ src/components/window/Playlist/PlaylistTab/SongList/index.jsx
@@ -29,7 +29,7 @@ class SongList extends Component {
                     if (i%2===0) {
                         res.push(<span>{ ar[i/2].name }</span>)
                     }else if(i!==(ar.length*2-1)) {
-                        res.push(<span>/</span>)
+                        res.push(<span> / </span>)
                     }
                 }
                 return res
@@ -37,18 +37,46 @@ class SongList extends Component {
 
             return (
                 <div className={ Style['list-item'] }>
-                    <span className={ Style['list-item-index'] }>{ index+1>=10?index+1:`0${index+1}` }</span>
-                    <span className={ Style['list-item-name'] }>{ name }</span>
-                    <span className={ Style['list-item-singers'] }>{ singer() }</span>
-                    <span className={ Style['list-item-album'] }>{ al.name }</span>
-                    <span className={ Style['list-item-time'] }>{ getTime() }</span>
+                    <div className={ Style['list-item-index-box'] }>
+                        <span className={ Style['list-item-index'] }>{ index+1>=10?index+1:`0${index+1}` }</span>
+                    </div>
+                    <div className={ Style['list-item-name-box'] }>
+                        <span className={ Style['list-item-name'] }>{ name }</span>
+                    </div>
+                    <div className={ Style['list-item-singers-box'] }>
+                        <span className={ Style['list-item-singers'] }>{ singer() }</span>
+                    </div>
+                    <div className={ Style['list-item-album-box'] }>
+                        <span className={ Style['list-item-album'] }>{ al.name }</span>
+                    </div>
+                    <span className={ Style['list-item-time-box'] }>
+                        <span className={ Style['list-item-time'] }>{ getTime() }</span>
+                    </span>
                 </div>
             )
         })
 
         return (
             <div>
-                <div></div>
+                <div>
+                    <div className={ Style['list-item-header'] }>
+                        <div className={ Style['list-item-index-header'] }>
+                            <span className={ Style['list-item-index'] }>#</span>
+                        </div>
+                        <div className={ Style['list-item-name-header'] }>
+                            <span className={ Style['list-item-name'] }>音乐标题</span>
+                        </div>
+                        <div className={ Style['list-item-singers-header'] }>
+                            <span className={ Style['list-item-singers'] }>歌手</span>
+                        </div>
+                        <div className={ Style['list-item-album-header'] }>
+                            <span className={ Style['list-item-album'] }>专辑</span>
+                        </div>
+                        <span className={ Style['list-item-time-header'] }>
+                            <span className={ Style['list-item-time'] }>时长</span>
+                        </span>
+                    </div>
+                </div>
                 <div>{ List }</div>
             </div>
         )
